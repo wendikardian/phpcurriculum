45c3a4ac82f9d5226041d000bb04f45507cff5d2
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-04-21 12:31:09 +0700

    finished flowe control

--- looping/break_con.php
+++ looping/break_con.php
@@ -1,4 +1,23 @@
 <?php
+    $numbers = ['three', 'four', 'two', 'six', 'seven'];
 
+    foreach($numbers as $number) {
+        if($number == 'six'){
+            break;
+        }
+        echo "The number is {$number} <br>";
+
+    }
+
+    echo "<br>";
+    echo 'using continue <br>';
+    foreach($numbers as $number) {
+        if($number == 'six'){
+            echo 'Continue ! <br>';
+            continue;
+        }
+        echo "The number is {$number} <br>";
+
+    }
 
 ?>
