0451fa8dc8fb941a776fe4d135f2bbc6a692aa3c
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-04-21 15:51:53 +0700

    updated

--- function/exercise.php
+++ function/exercise.php
@@ -20,4 +20,24 @@
 
     $activity = my_activity(false);
     echo $activity;
+
+    function fav_activity(string $name, bool $isRain = true) : string {
+        if($isRain){
+            return "Hei {$name}, do indoor exercise for your health";
+        }else{
+            return "{$name}, go out make interaction with others and jogging for the five miles!";
+        }
+
+    }
+
+
+    $act = fav_activity("Simon");
+    echo "<br>";
+    echo "<br>";
+    echo $act;
+    echo "<br>";
+    $act2 = fav_activity("Simon", false);
+    echo $act2;
+    echo "<br>";
+
 ?>
