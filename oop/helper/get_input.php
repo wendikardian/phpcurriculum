9b164469cfd0124e01f5558f663a68a750d96cf4
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-05-26 20:03:56 +0700

    update

--- oop/helper/get_input.php
+++ oop/helper/get_input.php
@@ -1,11 +1,11 @@
 <?php
-
-
 function input_checker($input_name, $default_value)
 {
+    // Mengecek apakah inputnya sudah dimasukan oleh user atau tidak dengan menggunakan Null Coalescing Operator
+    // Apabila sudah ada inputnya, maka akan mengembalikan nilai inputnya
+    // Apabila belum ada inputnya, maka akan mengembalikan nilai defaultnya
+    // Function akan dipanggil di class_medical_report.php
     $input_value = $_GET[$input_name] ?? $default_value;
     return $input_value;
 }
-
-
 ?>
